ad364a41177648b897528b8daf3f8da26e692e39
Rodrigo Melo <nalajejazz@gmail.com>
2022-06-02 14:34:35 -0300

    Inicio dos estudos

---
+++ src/app/componente1/componente1.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente1',
+  templateUrl: './componente1.component.html',
+  styleUrls: ['./componente1.component.css']
+})
+export class Componente1Component implements OnInit {
+
+  public titulo="Eu vou vencer"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
