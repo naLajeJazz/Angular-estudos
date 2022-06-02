523afa099b26172a67e8ba3305200b28a7a150a8
Rodrigo Melo <nalajejazz@gmail.com>
2022-06-02 17:03:02 -0300

    Event Biding

--- src/app/componente1/componente1.component.ts
+++ src/app/componente1/componente1.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Componente1Component implements OnInit {
 
-  public titulo:string="Eu vou vencer"
+  public titulo:string="Clique na bolinha";
+  public jumpBall ="https://piskel-imgstore-b.appspot.com/img/c8ec5978-e2ac-11ec-bdbc-8d97f02f2eb1.gif"
+  
+public muda=()=>{
+  this.jumpBall="https://piskel-imgstore-b.appspot.com/img/f8376a02-e2a6-11ec-b05e-8d97f02f2eb1.gif"
+}
 
   constructor() { }
 
