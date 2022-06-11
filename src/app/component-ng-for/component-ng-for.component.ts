dfea3c4129e21e14c05c1a36b1f02523eb7498ea
Rodrigo Melo <nalajejazz@gmail.com>
2022-06-10 21:16:50 -0300

    ngFor e splice em um item da lista

--- src/app/component-ng-for/component-ng-for.component.ts
+++ src/app/component-ng-for/component-ng-for.component.ts
@@ -8,13 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class ComponentNgForComponent implements OnInit {
 
   public card:Boolean=false;
-  public lista:Array<{nome:string,valor:Number}>=[
+  public lista:Array<{nome:string,valor:number}>=[
     {nome:"x-box",valor:100},
     {nome:"playstation",valor:500},
     {nome:"nintendo",valor:100}]
   
   public activeCard=()=>{if(!this.card){this.card=true}else{this.card=false}};
-  public add=()=>{this.lista.push({nome:"atari",valor:1000}) }
+  public add=()=>{this.lista.push({nome:"atari",valor:1000}) };
+  public eventAdd=(event:number)=>{this.lista.splice(event,1)}
 
 
   constructor() { }
